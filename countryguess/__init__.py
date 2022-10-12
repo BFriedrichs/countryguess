1a5aba29ad6a651113607b6041cada2ac74b30c4
plotski <plotski@example.org>
2022-10-12 11:52:18 +0200

    __init__.py: Prevent circular import

--- countryguess/__init__.py
+++ countryguess/__init__.py
@@ -1,9 +1,9 @@
-from ._countrydata import CountryData
-from ._guess_country import guess_country
-
 __project_name__ = 'countryguess'
 __description__ = 'Fuzzy lookup of country information'
 __homepage__ = 'https://codeberg.org/plotski/countryguess'
 __version__ = '0.0.1'
 __author__ = 'plotski'
 __author_email__ = 'plotski@example.org'
+
+from ._countrydata import CountryData
+from ._guess_country import guess_country
